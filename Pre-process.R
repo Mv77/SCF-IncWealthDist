15611a45f79a5d3b38496c67b5e09f014803035d
MateoVG <mateovegi@gmail.com>
2021-01-15 16:02:13 -0500

    Split scripts and export csv

--- Pre-process.R
+++ Pre-process.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(srvyr)
 library(haven)
 library(forcats)
 
@@ -64,45 +63,4 @@ SCF <- SCF %>% filter(Wealth > 0,
                       PermIncome > 0) %>%
   drop_na(YEAR, Educ, Age_grp)
 
-# Summary statistics ----
-
-# Create survey object, and group it.
-scf_srvy <- as_survey_design(SCF, weights = 'WEIGHT')
-
-# Define groupings
-groupings <- list(c("Educ", "YEAR", "Age_grp"),
-                  c("YEAR", "Age_grp"),
-                  c("Educ", "Age_grp"),
-                  c("Educ", "YEAR"),
-                  c("Educ"), c("YEAR"), c("Age_grp"))
-
-# An auxiliary function that finds the quantities of
-# interest at any grouping
-calcSumStats <- function(survey, grouping){
-  
-  # Find number of observations used for stats
-  sumstats <- survey %>% group_by_at(grouping) %>%
-    srvyr::summarise(
-      
-      # Weighted and unweighted number of observations.
-      w.obs = survey_total(),
-      obs   = unweighted(n()),
-      
-      # Mean and sd of log(Wealth/Permanent income)
-      lnNrmWealth.mean = survey_mean(lnNrmWealth),
-      lnNrmWealth.sd   = survey_sd(lnNrmWealth),
-      
-      # Mean and sd of log(Permanent income)
-      lnPermIncome.mean = survey_mean(lnPermIncome),
-      lnPermIncome.sd   = survey_sd(lnPermIncome)
-      
-    ) %>%
-    select(-contains("_se")) 
-  
-  return(sumstats)
-  
-}
-
-# Apply to all groupings and combine into single table.
-table <- lapply(groupings, function(grp) calcSumStats(scf_srvy, grp)) %>%
-  bind_rows()
+rm(list = setdiff(ls(), c('SCF','scripts_dir')))
---
+++ do_all.R
@@ -0,0 +1,3 @@
+scripts_dir <- "C:/GitHub/SCF-IncWealthDist"
+source(file.path(scripts_dir,'Pre-process.R'))
+source(file.path(scripts_dir,'Summ-stats.R'))
